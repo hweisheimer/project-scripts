f277a4d1f904a4b81b2b82b0f86da6993511d494
Hans Weisheimer <hweisheimer@users.noreply.github.com>
2024-04-30 15:30:56 -0400

    git config and GCM fixes

--- git-config.sh
+++ git-config.sh
@@ -8,10 +8,15 @@ pass init hansw
 git config --global user.name "Hans Weisheimer"
 git config --global user.email "hweisheimer@users.noreply.github.com"
 git config --global credential.credentialStore gpg
+git config --global init.defaultBranch main
+
+alias gs="git status"
 
 curl -fsSL https://github.com/git-ecosystem/git-credential-manager/releases/download/v2.5.0/gcm-linux_amd64.2.5.0.deb -o gcm-linux.deb
 dpkg -i gcm-linux.deb
 rm gcm-linux.deb
+
+git credential-manager configure
 
 git credential-manager github login
 
