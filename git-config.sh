d6a07f80c3af1d27b988e4044ef7520a924f11b3
Hans Weisheimer <hweisheimer@users.noreply.github.com>
2024-04-30 16:09:14 -0400

    shell settings

--- git-config.sh
+++ git-config.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-apt-get install -y git gpg pass apt-transport-https ca-certificates curl
+apt-get install -y gpg pass apt-transport-https ca-certificates curl
 
 gpg --quick-generate-key hansw
 pass init hansw
@@ -9,8 +9,6 @@ git config --global user.name "Hans Weisheimer"
 git config --global user.email "hweisheimer@users.noreply.github.com"
 git config --global credential.credentialStore gpg
 git config --global init.defaultBranch main
-
-alias gs="git status"
 
 curl -fsSL https://github.com/git-ecosystem/git-credential-manager/releases/download/v2.5.0/gcm-linux_amd64.2.5.0.deb -o gcm-linux.deb
 dpkg -i gcm-linux.deb
